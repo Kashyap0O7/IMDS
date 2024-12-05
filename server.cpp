ff43c5c8402bf0f70a8978a3c9878811e763f973
JV612 <vaghasiyajay54@gmail.com>
2024-12-05 19:05:34 +0530

    add write_full funct to handle write buffer full issue

--- server.cpp
+++ server.cpp
@@ -21,13 +21,26 @@ static void die(const char *msg) {
 }
 
 const size_t k_max_msg = 4096;
-char *buf[4+k_max_msg];
+char buf[4+k_max_msg];
 
 static int32_t read_full(int fd, char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = read(fd, buf, n);
         if (rv <= 0) {
             return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+int write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
         }
         assert((size_t)rv <= n);
         n -= (size_t)rv;
