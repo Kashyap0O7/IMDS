55f06dd39ee984bf69cfedf3a181675a9b7064bf
kashyap0O7 <kapansuriya007@gmail.com>
2024-12-16 12:00:31 +0530

    implement hmap using two htables

--- hashtable.cpp
+++ hashtable.cpp
@@ -37,3 +37,75 @@ static HNode *h_detach(HTab *htab, HNode **from) {
     htab->size--;
     return node;
 }
+
+const size_t k_rehashing_work = 128;
+
+static void hm_help_rehashing(HMap *hmap) {
+    size_t nwork = 0;
+    while (nwork < k_rehashing_work && hmap->older.size > 0) {
+        HNode **from = &hmap->older.tab[hmap->migrate_pos];
+        if (!*from) {
+            hmap->migrate_pos++;
+            continue;
+        }
+        h_insert(&hmap->newer, h_detach(&hmap->older, from));
+        nwork++;
+    }
+    if (hmap->older.size == 0 && hmap->older.tab) {
+        free(hmap->older.tab);
+        hmap->older = HTab{};
+    }
+}
+
+static void hm_trigger_rehashing(HMap *hmap) {
+    assert(hmap->older.tab == NULL);
+    hmap->older = hmap->newer;
+    h_init(&hmap->newer, (hmap->newer.mask + 1) * 2);
+    hmap->migrate_pos = 0;
+}
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *)) {
+    hm_help_rehashing(hmap);
+    HNode **from = h_lookup(&hmap->newer, key, eq);
+    if (!from) {
+        from = h_lookup(&hmap->older, key, eq);
+    }
+    return from ? *from : NULL;
+}
+
+const size_t k_max_load_factor = 8;
+
+void hm_insert(HMap *hmap, HNode *node) {
+    if (!hmap->newer.tab) {
+        h_init(&hmap->newer, 4);
+    }
+    h_insert(&hmap->newer, node);
+    if (!hmap->older.tab) {
+        size_t shreshold = (hmap->newer.mask + 1) * k_max_load_factor;
+        if (hmap->newer.size >= shreshold) {
+            hm_trigger_rehashing(hmap);
+        }
+    }
+    hm_help_rehashing(hmap);
+}
+
+HNode *hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *)) {
+    hm_help_rehashing(hmap);
+    if (HNode **from = h_lookup(&hmap->newer, key, eq)) {
+        return h_detach(&hmap->newer, from);
+    }
+    if (HNode **from = h_lookup(&hmap->older, key, eq)) {
+        return h_detach(&hmap->older, from);
+    }
+    return NULL;
+}
+
+void hm_clear(HMap *hmap) {
+    free(hmap->newer.tab);
+    free(hmap->older.tab);
+    *hmap = HMap{};
+}
+
+size_t hm_size(HMap *hmap) {
+    return hmap->newer.size + hmap->older.size;
+}
