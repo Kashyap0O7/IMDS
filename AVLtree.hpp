94fe7f562ad3ddbb6ece8b4c47cf731894fc047b
JV612 <vaghasiyajay54@gmail.com>
2024-12-25 12:03:45 +0530

    avl tree minor changes and header file

--- AVLtree.hpp
+++ AVLtree.hpp
@@ -12,3 +12,16 @@ struct AVLNode {
     uint32_t cnt = 0;       
 };
 
+inline void avl_init(AVLNode *node) {
+    node->left = node->right = node->parent = NULL;
+    node->height = 1;
+    node->cnt = 1;
+}
+
+
+inline uint32_t avl_height(AVLNode *node) { return node ? node->height : 0; }
+inline uint32_t avl_cnt(AVLNode *node) { return node ? node->cnt : 0; }
+
+
+AVLNode *avl_fix(AVLNode *node);
+AVLNode *avl_del(AVLNode *node);
