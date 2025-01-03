540794e69d0bd2756fe2ee34ecd8a1d89286cf2d
kashyap0O7 <kapansuriya007@gmail.com>
2025-01-03 12:13:22 +0530

    sorted set query functions add

--- Sorted_Set.cpp
+++ Sorted_Set.cpp
@@ -1,11 +1,22 @@
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
+#include <string.h>
 #include "Sorted_Set.hpp"
 #include "usual.hpp"
 
+
+struct HKey {
+    HNode node;
+    const char *name = NULL;
+    size_t len = 0;
+};
+
 static void ssnode_del(SSNode *node) {
     free(node);
+}
+
+static size_t min_node(size_t lhs, size_t rhs) {
+    return lhs < rhs ? lhs : rhs;
 }
 
 
@@ -21,9 +32,11 @@ static SSNode *ssnode_new(const char *name, size_t len, double score) {
     return node;
 }
 
-static size_t min_node(size_t lhs, size_t rhs) {
-    return lhs < rhs ? lhs : rhs;
+static bool ssless(AVLNode *lhs, AVLNode *rhs) {
+    SSNode *zr = container_of(rhs, SSNode, tree);
+    return ssless(lhs, zr->score, zr->name, zr->len);
 }
+
 
 static bool ssless(AVLNode *lhs, double score, const char *name, size_t len){
     SSNode *zl = container_of(lhs, SSNode, tree);
@@ -37,19 +50,56 @@ static bool ssless(AVLNode *lhs, double score, const char *name, size_t len){
     return zl->len < len;
 }
 
+static void tree_insert(Sorted_Set *sset, SSNode *node) {
+    AVLNode *parent = NULL;         
+    AVLNode **from = &sset->root;   
+    while (*from) {                 
+        parent = *from;
+        from = ssless(&node->tree, parent) ? &parent->left : &parent->right;
+    }
+    *from = &node->tree;            
+    node->tree.parent = parent;
+    sset->root = avl_fix(&node->tree);
+}
+
 
 static void sset_update(Sorted_Set *sset, SSNode *node, double score) {
     if (node->score == score) {
         return;
     }
-
+    sset->root = avl_del(&node->tree);
+    avl_init(&node->tree);
+    node->score = score;
+    tree_insert(sset, node);
 }
 
-struct HKey {
-    HNode node;
-    const char *name = NULL;
-    size_t len = 0;
-};
+bool sset_insert(Sorted_Set *sset, const char *name, size_t len, double score) {
+    SSNode *node = sset_lookup(sset, name, len);
+    if (node) {
+        sset_update(sset, node, score);
+        return false;
+    } else {
+        node = ssnode_new(name, len, score);
+        hm_insert(&sset->hmap, &node->hmap);
+        tree_insert(sset, node);
+        return true;
+    }
+}
+
+
+void sset_delete(Sorted_Set *sset, SSNode *node) {
+    
+    HKey key;
+    key.node.hashcode = node->hmap.hashcode;
+    key.name = node->name;
+    key.len = node->len;
+    HNode *found = hm_delete(&sset->hmap, &key.node, &hcmp);
+    assert(found);
+    
+    sset->root = avl_del(&node->tree);
+    ssnode_del(node);
+}
+
 
 static bool hcmp(HNode *node, HNode *key) {
     SSNode *ssnode = container_of(node, SSNode, hmap);
@@ -74,25 +124,16 @@ SSNode *sset_lookup(Sorted_Set *sset, const char *name, size_t len) {
 }
 
 
-void sset_delete(Sorted_Set *sset, SSNode *node) {
-    
-    HKey key;
-    key.node.hashcode = node->hmap.hashcode;
-    key.name = node->name;
-    key.len = node->len;
-    HNode *found = hm_delete(&sset->hmap, &key.node, &hcmp);
-    assert(found);
-    
-    sset->root = avl_del(&node->tree);
-    ssnode_del(node);
-}
-
 SSNode *sset_seekge(Sorted_Set *sset, double score, const char *name, size_t len) {
     AVLNode *found = NULL;
     for (AVLNode *node = sset->root; node; ) {
         if (ssless(node, score, name, len)) {
             node = node->right; 
-        } 
+        } else {
+            found = node;       
+            node = node->left;
+        }
+    }
     return found ? container_of(found, SSNode, tree) : NULL;
 }
 
@@ -100,4 +141,20 @@ SSNode *sset_seekge(Sorted_Set *sset, double score, const char *name, size_t len
 SSNode *ssnode_offset(SSNode *node, int64_t offset) {
     AVLNode *tnode = node ? avl_off_set(&node->tree, offset) : NULL;
     return tnode ? container_of(tnode, SSNode, tree) : NULL;
+}
+
+static void tree_dispose(AVLNode *node) {
+    if (!node) {
+        return;
+    }
+    tree_dispose(node->left);
+    tree_dispose(node->right);
+    ssnode_del(container_of(node, SSNode, tree));
+}
+
+
+void sset_clear(Sorted_Set *sset) {
+    hm_clear(&sset->hmap);
+    tree_dispose(sset->root);
+    sset->root = NULL;
 }
