fd7039ea18ac0c253e8c1b108167e28c9293e51c
JV612 <vaghasiyajay54@gmail.com>
2025-01-05 10:27:32 +0530

    avl tree code changes and fixes

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -1,22 +1,43 @@
 #include <assert.h>
+
 #include "AVLtree.hpp"
 
-static uint32_t max(uint32_t lhs, uint32_t rhs) {
+static uint32_t max_node(uint32_t lhs, uint32_t rhs) {
     return lhs < rhs ? rhs : lhs;
 }
 
 static void avl_update(AVLNode *node) {
-    node->height = 1 + max(avl_height(node->left), avl_height(node->right));
+    node->height = 1 + max_node(avl_height(node->left), avl_height(node->right));
     node->cnt = 1 + avl_cnt(node->left) + avl_cnt(node->right);
 }
 
-static AVLNode *rot_left(AVLNode *node) {
+static AVLNode *rotate_right(AVLNode *node) {
+    AVLNode *parent = node->parent;
+    AVLNode *new_node = node->left;
+    AVLNode *inner = new_node->right;
+
+    node->left = inner;
+    if (inner) {
+        inner->parent = node;
+    }
+    new_node->parent = parent;
+    new_node->right = node;
+    node->parent = new_node;
+    
+    avl_update(node);
+    avl_update(new_node);
+    return new_node;
+}
+
+static AVLNode *rotate_left(AVLNode *node) {
     AVLNode *parent = node->parent;
     AVLNode *new_node = node->right;
     AVLNode *inner = new_node->left;
+
     node->right = inner;
     if (inner) {
         inner->parent = node;
+    
     }
     new_node->parent = parent;
     new_node->left = node;
@@ -26,51 +47,41 @@ static AVLNode *rot_left(AVLNode *node) {
     return new_node;
 }
 
-static AVLNode *rot_right(AVLNode *node) {
-    AVLNode *parent = node->parent;
-    AVLNode *new_node = node->left;
-    AVLNode *inner = new_node->right;
-    node->left = inner;
-    if (inner) {
-        inner->parent = node;
+static AVLNode *avl_fix_right(AVLNode *node){
+    if (avl_height(node->right->right) < avl_height(node->right->left)) {
+        node->right = rotate_right(node->right);
     }
-    new_node->parent = parent;
-    new_node->right = node;
-    node->parent = new_node;
-    avl_update(node);
-    avl_update(new_node);
-    return new_node;
+
+    return rotate_left(node);
 }
 
 static AVLNode *avl_fix_left(AVLNode *node) {
     if (avl_height(node->left->left) < avl_height(node->left->right)) {
-        node->left = rot_left(node->left);
+        node->left = rotate_left(node->left);
     }
-    return rot_right(node);
-}
-
-static AVLNode *avl_fix_right(AVLNode *node) {
-    if (avl_height(node->right->right) < avl_height(node->right->left)) {
-        node->right = rot_right(node->right);
-    }
-    return rot_left(node);
+    return rotate_right(node);
 }
 
 AVLNode *avl_fix(AVLNode *node) {
     while (true) {
         AVLNode **from = &node;
         AVLNode *parent = node->parent;
+
         if (parent) {
-            from = parent->left == node ? &parent->left : &parent->right;
+            from = ((parent->left == node) ? (&parent->left) : (&parent->right));
         }
+
         avl_update(node);
-        uint32_t l = avl_height(node->left);
-        uint32_t r = avl_height(node->right);
-        if (l == r + 2) {
+        uint32_t le = avl_height(node->left);
+        uint32_t ri = avl_height(node->right);
+
+        if (le == ri + 2) {
             *from = avl_fix_left(node);
-        } else if (l + 2 == r) {
+        } else if (le + 2 == ri) {
             *from = avl_fix_right(node);
         }
+
+
         if (!parent) {
             return *from;
         }
@@ -78,42 +89,76 @@ AVLNode *avl_fix(AVLNode *node) {
     }
 }
 
-static AVLNode *avl_del_easy(AVLNode *node) {
+AVLNode *avl_off_set(AVLNode *node, int64_t offset) {
+    int64_t pos = 0;
+    while (offset != pos) {
+        if (pos < offset && pos + avl_cnt(node->right) >= offset) {
+            node = node->right;
+            pos += avl_cnt(node->left) + 1;
+        } 
+        else if(pos > offset && pos - avl_cnt(node->left) <= offset) {
+            node = node->left;
+            pos -= avl_cnt(node->right) + 1;
+        } 
+        else{
+            AVLNode *parent = node->parent;
+            if (!parent) return NULL;
+            if (parent->right == node) {
+                pos -= avl_cnt(node->left) + 1;
+            } else {
+                pos += avl_cnt(node->right) + 1;
+            }
+            node = parent;
+        }
+    }
+
+    return node;
+}
+
+static AVLNode *avl_lazy_del(AVLNode *node) {
     assert(!node->left || !node->right);
+
     AVLNode *child = node->left ? node->left : node->right;
     AVLNode *parent = node->parent;
+
     if (child) {
         child->parent = parent;
     }
     if (!parent) {
         return child;
     }
+
     AVLNode **from = parent->left == node ? &parent->left : &parent->right;
+
     *from = child;
     return avl_fix(parent);
 }
 
 AVLNode *avl_del(AVLNode *node) {
-    if (!node->left || !node->right) {
-        return avl_del_easy(node);
-    }
+    if (!node->left || !node->right) return avl_lazy_del(node);
     AVLNode *victim = node->right;
+
     while (victim->left) {
         victim = victim->left;
     }
-    AVLNode *root = avl_del_easy(victim);
+
+    AVLNode *root = avl_lazy_del(victim);
     *victim = *node;
+
     if (victim->left) {
         victim->left->parent = victim;
     }
     if (victim->right) {
         victim->right->parent = victim;
     }
+
     AVLNode **from = &root;
     AVLNode *parent = node->parent;
+
     if (parent) {
         from = parent->left == node ? &parent->left : &parent->right;
     }
+
     *from = victim;
     return root;
 }
--- AVLtree.hpp
+++ AVLtree.hpp
@@ -4,24 +4,25 @@
 #include <stdint.h>
 
 
-struct AVLNode {
+struct 
+AVLNode {
     AVLNode *parent = NULL;
-    AVLNode *left = NULL;
     AVLNode *right = NULL;
-    uint32_t height = 0;    
+    AVLNode *left = NULL;
     uint32_t cnt = 0;       
+    uint32_t height = 0;   
 };
 
-inline void avl_init(AVLNode *node) {
+inline 
+void avl_init(AVLNode *node) {
     node->left = node->right = node->parent = NULL;
     node->height = 1;
     node->cnt = 1;
 }
 
-
-inline uint32_t avl_height(AVLNode *node) { return node ? node->height : 0; }
 inline uint32_t avl_cnt(AVLNode *node) { return node ? node->cnt : 0; }
+inline uint32_t avl_height(AVLNode *node) { return node ? node->height : 0; }
 
-
-AVLNode *avl_fix(AVLNode *node);
 AVLNode *avl_del(AVLNode *node);
+AVLNode *avl_fix(AVLNode *node);
+AVLNode *avl_off_set(AVLNode *node, int64_t offset);
